e4bc319a5ec798515ec2323ca58ab4f2d094bc00
Teemu Sipiläinen <tsipilainen@yahoo.com>
2025-08-24 17:21:07 +0300

    Add Hello World project for Section 2

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * Udemy course
+ * 
+ * C# Basics for Beginners: Learn C# Fundamentals by Coding
+ * Section 2: Introduction to C# and .NET Framework
+ * 11. Our First C# Application
+ * Project: Hello World
+ * 
+ * Author: Teemu Sipiläinen
+ * Date: 24 Aug, 2025
+ */
+
+using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
